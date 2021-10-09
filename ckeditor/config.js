871259a5f380c8f9a44fc2e31d4b58aed108fa3b
Luiz Fernando Brogliatto Ferreira <luiz0067@gmail.com>
2021-10-09 10:07:31 -0300

    Add files via upload

---
+++ ckeditor/config.js
@@ -0,0 +1,12 @@
+﻿/*
+Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	
+};
+    window.onload = function() {
+        CKEDITOR.replace( 'editor1', {enterMode: Number(2)} );
+      };
